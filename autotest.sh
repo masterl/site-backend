82ff0a3e0a3bf914be6b4e74b793f20a13a9d461
Leonardo de Oliveira Lourenço <bigodation@gmail.com>
2019-09-10 20:10:49 -0700

    Added automated testing script

---
+++ autotest.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+readonly PROJECT_ROOT=$( cd "$( dirname "$0" )" && pwd )
+
+function main()
+{
+  while true; do
+    find "$PROJECT_ROOT" -type f |
+      grep -v -E "^$PROJECT_ROOT/node_modules" |
+      grep -v -E "^$PROJECT_ROOT/.git" |
+      entr -d bash entr_script.sh "$PROJECT_ROOT"
+  done
+}
+
+main "$@"
