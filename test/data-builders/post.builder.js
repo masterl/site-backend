f7900b186bf1a8151e7ebb58583b526870a91e54
Leonardo de Oliveira Lourenço <bigodation@gmail.com>
2019-09-12 19:02:02 -0700

    Improved PostBuilder to ensure body is large enough

--- test/data-builders/post.builder.js
+++ test/data-builders/post.builder.js
@@ -1,8 +1,11 @@
+
+const generate_large_text = () => faker.lorem.words(150);
+
 module.exports = class PostBuilder {
   static random_post_info () {
     return {
       title: faker.lorem.sentence(),
-      body:  faker.lorem.text()
+      body:  generate_large_text()
     };
   }
 };
